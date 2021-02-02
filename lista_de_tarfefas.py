23be9633434a6647837325d486ddca9f11af3e89
Silvia Michele <SilviaMLTeles@gmail.com>
2021-02-02 11:18:06 -0200

    update

--- lista_de_tarfefas.py
+++ lista_de_tarfefas.py
@@ -1,41 +1,60 @@
 
 # adicionar, remover, refazer
+modify = 'a'
+todo_list = []
 
-def listar(a):
-    for x in range(len(a)):
-        print(f'{x+1} - {a[x]}')
+def modify_global(value):
+    global modify
+    modify = value
+
+def show_todo(todo_list):
+    for x in range(len(todo_list)):
+        print(f'{x+1} - {todo_list[x]}')
+
+def add_todo(todo_list):
+    add_ask = input('Nova tarefa: ')
+
+    modify_global(add_ask)  # guarda a modificacao
+
+    todo_list.append(add_ask)  #adiciona a tarefa na lista
+    print('Tarefa adicionada.')
+
+
+def remove_todo(todo_list):
+    show_todo(todo_list)
+    remove_ask = int(input('Número da tarefa que deseja remover: '))
+
+    modify_global(todo_list[remove_ask-1])
+
+    todo_list.pop(remove_ask-1)  #remove a tarefa desejada
+    print('Tarefa removida.')
+
+def modify_todo(todo_list):
+
+    if modify in todo_list:
+        todo_list.remove(modify)  #remove o ultimo valor adicionado
+    else:
+        todo_list.append(modify)  #adiciona o ultimo valor removido
 
 if __name__ == "__main__":
-    lista_tarefas = []
-    modificacao = ''
 
-    menu = '(1) Nova Tarefa\n(2) Remover tarefa\n(3) Desfazer última alteração\n(4) Listar as tarefas\nEscolha: '
+    menu = 'Digite 0 para finalizar.\n(1) Nova Tarefa\n(2) Remover tarefa\n(3) Desfazer última alteração\n(4) Listar as tarefas\nEscolha: '
 
     while True:
 
-        opcao = int(input(menu))  #imprime a variavel menu e recebe a opção desejada pelo usuario
+        ask = int(input(menu))  #imprime a variavel menu e recebe a opção desejada pelo usuario
 
-        if opcao == 1:
-            opcao2 = input('Nova tarefa: ')
-            modificacao = opcao2  #salva a nova tarefa para uma futura modificacao
-            lista_tarefas.append(opcao2)  #insere uma nova tarefa
-            print('Tarefa adicionada.')
+        if ask == 1:
+            add_todo(todo_list)
 
-        elif opcao == 2:
-            listar(lista_tarefas)
-            opcao2 = int(input('Qual tarefa deseja remover:  '))
-            modificacao = lista_tarefas[opcao2-1]  #salva a tarefa removida para uma futura modificacao
-            lista_tarefas.pop(opcao2-1)   #exclui a tarefa removida
-            print('Tarefa excluida.')
+        elif ask == 2:
+            remove_todo(todo_list)
 
-        elif opcao == 3:
-            if modificacao in lista_tarefas:
-                lista_tarefas.remove(modificacao)  #remove o ultimo valor adicionado
-            else:
-                lista_tarefas.append(modificacao)   #insere o ultimo valor removido
+        elif ask == 3:
+            modify_todo(todo_list)
 
+        elif ask == 4:
+            show_todo(todo_list)
         else:
-            print('*'*20)
-            listar(lista_tarefas)
-            print('*' * 20)
+            break
 
